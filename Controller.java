a5eff5c96e9a89a075bddd9aadc3572275156da7
Yaro <ysboyko@gmail.com>
2019-04-01 23:37:37 -0400

    One last time

---
+++ Controller.java
@@ -0,0 +1,13 @@
+package BeltLineApplication;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label helloWorld;
+    public Label RegisterUser;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
