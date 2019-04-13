0d57e7ccf260e68b0c9ef2d62e036e93422b9a59
Julia Corona <juliacorona@live.com>
2019-04-13 14:16:23 -0400

    Added Models

--- java/model/AssignTo.java
+++ java/model/AssignTo.java
@@ -1,4 +1,40 @@
 package BeltLineApplication.java.model;
 
 public class AssignTo {
+    private String staffUsername;
+    private String eName;
+    private String startDate;
+    private String siteName;
+
+    public String getStaffUsername(){
+        return StaffUsername;
+    }
+
+    public String getEName(){
+        return EName;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public String getSiteName() {
+        return SiteName;
+    }
+
+    public void setStaffUsername(String staffUsername) {
+        this.staffUsername = staffUsername;
+    }
+
+    public void setEName(String eName) {
+        this.eName = eName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
 }
--- java/model/Take.java
+++ java/model/Take.java
@@ -1,4 +1,24 @@
 package BeltLineApplication.java.model;
 
 public class Take {
+    private String username;
+    private String route;
+    private String type;
+    private String date;
+
+    public String getUsername() { return username; }
+
+    public String getRoute() { return route; }
+
+    public String getType() { return type; }
+
+    public String getDate() { return date; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public void setRoute(String route) { this.route = route; }
+
+    public void setType(String type) { this.type = type; }
+
+    public void setDate(String date) { this.date = date; }
 }
