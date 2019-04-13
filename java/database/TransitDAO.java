7ad21f2bab4ffc9f3227b203d9cd41e89e6f97ab
Julia Corona <juliacorona@live.com>
2019-04-13 18:25:17 -0400

    Update TransitDAO.java

--- java/database/TransitDAO.java
+++ java/database/TransitDAO.java
@@ -1,4 +1,23 @@
 package BeltLineApplication.java.database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TransitDAO {
+    //not done
+    public static ResultSet filter(String site, String type, Double Price) throws SQLException, ClassNotFoundException {
+        String query =
+                "select * from transit where ;";
+        try {
+            ResultSet rs = Connector.dbExecuteQuery(query);
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Something is wrong with your SQL: " + e);
+            throw e;
+        }
+    }
+    //not done
+    public static ResultSet filter(String site, String type) throws SQLException{
+        return null;
+    }
 }
